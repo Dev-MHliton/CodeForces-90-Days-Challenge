1a80fd7efcba8a256575368737ac6bd34c80e3c3
Dev-MHliton <liton41126nk@gmail.com>
2026-09-01 23:47:12 +0600

    remvoe

---
+++ First Month-September/Day-02/A_Vasya_the_Hipster.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
+int main()
+{
+    fastio();
+
+    return 0;
+}
