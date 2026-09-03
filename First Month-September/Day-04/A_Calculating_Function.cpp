e187d88b1d2959d3ba4961b0b14741319736a57d
Dev-MHliton <liton41126nk@gmail.com>
2026-09-03 22:58:40 +0600

    nC

---
+++ First Month-September/Day-04/A_Calculating_Function.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
+int main()
+{
+    fastio();
+
+    int t = 1;
+    // cin >> t;
+
+    while (t--)
+    {
+
+        cout << "Hello CP!" << endl;
+    }
+
+    return 0;
+}
