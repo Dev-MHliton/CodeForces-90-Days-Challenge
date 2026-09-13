3c7b51e89306214308e1d180986ec135c05dbfdb
Dev-MHliton <liton41126nk@gmail.com>
2026-09-13 21:50:49 +0600

    new setup

--- First Month-September/Day-05/A_Black_Square.cpp
+++ First Month-September/Day-05/A_Black_Square.cpp
@@ -1,10 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
 int main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
+    fastio();
 
     int a[5];
     for (int i = 1; i <= 4; i++)
