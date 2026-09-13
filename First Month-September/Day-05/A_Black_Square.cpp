586055679b66f54300cc0e2cd95460848f8a70de
Dev-MHliton <liton41126nk@gmail.com>
2026-09-13 21:48:31 +0600

    comment out

--- First Month-September/Day-05/A_Black_Square.cpp
+++ First Month-September/Day-05/A_Black_Square.cpp
@@ -6,7 +6,7 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int a[5]; // index 1 থেকে 4 ব্যবহার করবো
+    int a[5];
     for (int i = 1; i <= 4; i++)
         cin >> a[i];
 
@@ -16,7 +16,7 @@ int main()
     long long total = 0;
     for (char c : s)
     {
-        int strip = c - '0'; // character থেকে সংখ্যা বের করা
+        int strip = c - '0';
         total += a[strip];
     }
 
