d16ef99a773ceb6c5ac1ddaf468d1382130fca58
Dev-MHliton <liton41126nk@gmail.com>
2026-09-14 23:58:10 +0600

    spaceS

--- First Month-September/Day-05/A_Love_Story.cpp
+++ First Month-September/Day-05/A_Love_Story.cpp
@@ -15,12 +15,15 @@ int main()
 
     int t;
     cin >> t;
+
     string target = "codeforces";
+
     while (t--)
     {
         string s;
         cin >> s;
         int diff = 0;
+
         for (int i = 0; i < 10; i++)
         {
             if (s[i] != target[i])
