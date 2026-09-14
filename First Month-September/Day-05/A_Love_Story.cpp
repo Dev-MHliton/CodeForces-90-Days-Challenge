b5fc06f790a2ac18f5cfe2d043e5acaf784cc02b
Dev-MHliton <liton41126nk@gmail.com>
2026-09-14 23:57:19 +0600

    setuN

--- First Month-September/Day-05/A_Love_Story.cpp
+++ First Month-September/Day-05/A_Love_Story.cpp
@@ -1,8 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define ll long long
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define pii pair<int, int>
+
 int main()
 {
+    fastio();
+
     int t;
     cin >> t;
     string target = "codeforces";
@@ -18,5 +28,6 @@ int main()
         }
         cout << diff << endl;
     }
+
     return 0;
 }
